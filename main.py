683eb5e4ac3a721605c345105ff6861294a30aa6
juanjose250489-hub <juanjose250489@gmail.com>
2026-09-19 01:46:37 -0300

    Load teams data into ArbolBST from JSON file
    Load JSON data from equipos.json into a binary search tree.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import json
 from estructuras.arbol_binario import ArbolBST
 
+with open("datos/equipos.json", "r", encoding="utf-8) as archivo:
+          lista_de_elementos = json.load(archivo)
+
+arbol = arbolBST()
+for elemento in lista_de_elementos:
+    arbol.insertar(elemento, clave=lambda e: e['nombre'].lower())
+
 from menu import menu_principal
 
 
